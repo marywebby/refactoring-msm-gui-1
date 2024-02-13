9e6cae92e22355149f6d51f59ec5aa4fc0e5f9e7
Mary Webby <118230483+marywebby@users.noreply.github.com>
2024-02-13 21:54:12 +0000

    added Director#filmography

--- app/models/director.rb
+++ app/models/director.rb
@@ -11,4 +11,22 @@
 #  updated_at :datetime         not null
 #
 class Director < ApplicationRecord
+  def filmography
+    my_id = self.id
+
+    matching_movies = Movie.where({ :director_id => self.id })
+
+    return matching_movies
+  end 
+
+  # Three 1-N (one to many) associations:
+  # - Director => Movie ]
+  # - Movies => Character 
+  # - Actor => Character 
+
+  # Try defining: 
+  # - Director#filmography 
+  # - Movie#director 
+  # - Movie#character 
+
 end
